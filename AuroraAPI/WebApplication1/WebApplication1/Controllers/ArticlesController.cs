fbd2ead5c60eeb671f6bf9e013b2d89cf5fb71aa
Moises(Roy) Garcia <61564831+MoytheBoyRoy@users.noreply.github.com>
2021-11-06 15:34:08 -0700

    Add files via upload

---
+++ AuroraAPI/WebApplication1/WebApplication1/Controllers/ArticlesController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArticlesController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IWebHostEnvironment _env;
+        public ArticlesController(IConfiguration configuration, IWebHostEnvironment env )
+        {
+            _configuration = configuration;
+            _env = env;
+        }
+        [HttpGet("{id}")]
+        public JsonResult Get(int id)
+        {
+            String query = @"
+                            Select * from dbo.officialArticles
+                            where articleID=@ArticleId;
+                            
+                            ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("ArticleCon");
+            SqlDataReader myReader;
+            using(SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using(SqlCommand myCommand = new SqlCommand(query,myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@ArticleId", id);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+
+        [Route("SaveFile")]
+        [HttpPost]
+        public JsonResult SaveFile()
+        {
+            try
+            {
+                var httpRequest = Request.Form;
+                var postedFile = httpRequest.Files[0];
+                string filename = postedFile.FileName;
+                var physicalPath = _env.ContentRootPath + "/Articles/" + filename;
+
+                using(var stream = new FileStream(physicalPath, FileMode.Create))
+                {
+                    postedFile.CopyTo(stream);
+                }
+
+                return new JsonResult(filename);
+            }
+            catch (Exception)
+            {
+                return new JsonResult("basic.html");
+            }
+        }
+
+    }
+}
---
+++ AuroraAPI/WebApplication1/WebApplication1/Models/officialArticles.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class officialArticles
+    {
+        public int ArticleID { get; set; }
+        public string DESCRIPTION { get; set; }
+        public string LastUpdate { get; set; }
+        public string Title { get; set; }
+        public string articlePath { get; set; }
+    }
+}
